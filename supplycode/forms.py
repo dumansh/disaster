5b58cfa9fa81b987cdd924b8c8e80dcb6d0cece6
joncd25 <joncdoong@gmail.com>
2022-06-08 12:23:03 -0400

    Added 'class form adding' html pages
    initialize new code submission pages
    initialize supply code app urls.py
    added supplies and equipment class forms

--- supplycode/forms.py
+++ supplycode/forms.py
@@ -6,3 +6,15 @@ class NewLaborClassForm(forms.Form):
     class Meta:
         model = Labor
         fields = ["labor_class", "billing_code", "default_rates", "active"]
+
+
+class NewSuppliesClassForm(forms.Form):
+    class Meta:
+        model = Labor
+        fields = ["supplies_class", "billing_code", "default_rates", "active"]
+
+
+class NewEquipmentClassForm(forms.Form):
+    class Meta:
+        model = Labor
+        fields = ["equipment_class", "billing_code", "default_rates", "active"]
